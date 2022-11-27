a17ef9b51176d50b9b40e88f9c6875c46875d363
Abby Van Auken <vanauken.abigail@gmail.com>
2022-11-27 09:19:54 -0700

    First commit. Initial project setup.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//Abby Trevino
+//CSC 160 Computer Science I
+//November 27th, 2022
+//Create the dice game "30 or Bust" where two players roll dice until one of them reaches 30.
+
+
+public class Main {
+}
