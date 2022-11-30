29a6be352cd545c2244dda823ba90e072a0f3c37
Abby Van Auken <vanauken.abigail@gmail.com>
2022-11-29 19:50:35 -0700

    Added rules and title screen with ASCII flair.

--- src/Game.java
+++ src/Game.java
@@ -14,12 +14,37 @@ public class Game {
 
     public void gameInit() {
         //display game title
+        System.out.println("\n" +
+                "\n" +
+                "    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////\n" +
+                "    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////\n" +
+                "    ////                                                                                                        ////\n" +
+                "    ////       _________     __________                    ______       _________      _______     _________    ////\n" +
+                "    ////      /____    /|   / _____  \\_\\   ______         / __   \\     /__    __/|    /  ____/|   /   _____/|   ////\n" +
+                "    ////      _|__/   / /  / /    /  / /  /  ___/|       / /   |  \\    |_ /  /|_|/   /  /|__|/   /   /____ |/   ////\n" +
+                "    ////     /___    / /  / /    /  / /   \\  \\__|/      / /    /  /|     /  / /     /  / /      /   _____/|     ////\n" +
+                "    ////    _|__/   / /  / / ___/  / /   __\\  \\        / /____/  / | ___/  /_/_    /  /_/__    /   /____ |/     ////\n" +
+                "    ////   /_______/ /   |________/ /   /_____/|      /_________/ / /_________/|  /_______/|  /________/|       ////\n" +
+                "    ////   |_______|/    |________|/    |_____|/      |_________|/  |_________|/  |_______|/  |________|/       ////\n" +
+                "    ////                                                                                                        ////\n" +
+                "    ////                                                                                                        ////\n" +
+                "    ////                                            PRESENTED  BY:                                              ////\n" +
+                "    ////                                                                                                        ////\n" +
+                "    ////                                            ABBY VAN AUKEN                                              ////\n" +
+                "    ////                                                                                                        ////\n" +
+                "    ////                                      2022 SEMESTER FINAL PROJECT                                       ////\n" +
+                "    ////                                                                                                        ////\n" +
+                "    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////\n" +
+                "    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////\n" +
+                "\n" +
+                "\n");
         //ask for player names and welcome them
         System.out.println("ENTER PLAYER ONE'S NAME: ");
         Player playerOne = new Player(gameReader.nextLine());
         System.out.println("ENTER PLAYER TWO'S NAME: ");
         Player playerTwo = new Player(gameReader.nextLine());
-        System.out.println("WELCOME " + playerOne.getName() + " AND " + playerTwo.getName() + "!");
+        //System.out.println("WELCOME " + playerOne.getName() + " AND " + playerTwo.getName() + "!");
+        System.out.println("\n    +-------------------- WELCOME " + playerOne.getName() + " AND " + playerTwo.getName() + "! --------------------+\n");
 
     }
 
@@ -29,7 +54,25 @@ public class Game {
             System.out.println("DO YOU WANT TO SEE THE RULES?\nY / N");
             String viewRules = gameReader.nextLine();
             if (viewRules.equals("y")) {
-                System.out.println("*Block of rules*");
+                System.out.println("\n    +-----------------------------------------------------------------------+\n" +
+                        "    |                                RULES                                  |\n" +
+                        "    +-----------------------------------------------------------------------+\n" +
+                        "    |                                                                       |\n" +
+                        "    |  EACH TURN:                                                           |\n" +
+                        "    |   A player will roll a pair of dice. They will then choose to         |\n" +
+                        "    |   add the value of one or both dice to their player score. If         |\n" +
+                        "    |   after this their score is not 30, the turn then passes to the       |\n" +
+                        "    |   next player.                                                        |\n" +
+                        "    |                                                                       |\n" +
+                        "    |  YOU WIN WHEN:                                                        |\n" +
+                        "    |   You are the first player to reach a score of exactly 30 points.     |\n" +
+                        "    |                                                                       |\n" +
+                        "    |  YOU \"BUST\" WHEN:                                                     |\n" +
+                        "    |   Your score goes over 30. If this happens your player score gets     |\n" +
+                        "    |   reset back to zero. The game continues.                             |\n" +
+                        "    |                                                                       |\n" +
+                        "    +-----------------------------------------------------------------------+\n");
+
                 validInput = true;
             } else if (viewRules.equals("n")) {
                 System.out.println("AH, SO YOU'VE BEEN HERE BEFORE. LET'S GET GOING THEN!");
